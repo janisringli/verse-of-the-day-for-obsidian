96a6fb70d7d6612c32c2e6cb42f8fdd1fe349494
Janis Ringli <janis.ringli@gmail.com>
2024-03-04 16:43:01 +0100

    first version of new plugin

---
+++ main.ts
@@ -0,0 +1,158 @@
+import { Plugin, Editor } from "obsidian";
+import { IncomingMessage } from "http";
+import * as https from "https";
+import { VerseSettingsTab } from "./settings";
+
+interface VerseSettings {
+	language: string;
+	outgoingLink: boolean;
+}
+const DEFAULT_SETTINGS: Partial<VerseSettings> = {
+	language: "en",
+	outgoingLink: true,
+};
+
+export default class VerseOfTheDayPlugin extends Plugin {
+	settings: VerseSettings;
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+	}
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+	async onload() {
+		await this.loadSettings();
+		//TODO: Make this a setting inside a config
+		this.addSettingTab(new VerseSettingsTab(this.app, this));
+		this.getVerseOfTheDay(this.settings.language).then((verseData) => {
+			if (verseData) {
+				console.log(verseData.verse);
+				console.log(verseData.index);
+			}
+		});
+
+		this.addCommand({
+			id: "insert-verse-of-the-day",
+			name: "Insert Verse of the Day",
+			editorCallback: async (editor: Editor) => {
+				const verseData = await this.getVerseOfTheDay(
+					this.settings.language
+				);
+				let book;
+				let number;
+				if (verseData) {
+					if (this.settings.outgoingLink == true) {
+						//TODO: Make this a setting inside a config
+						if (
+							Array.from(verseData.index)[0] == "1" ||
+							Array.from(verseData.index)[0] == "2" ||
+							Array.from(verseData.index)[0] == "3"
+						) {
+							number = verseData.index.split(" ")[0];
+							book =
+								"[[" +
+								number +
+								". " +
+								verseData.index.split(" ")[1] +
+								"]]";
+						} else {
+							book = "[[" + verseData.index.split(" ")[0] + "]]";
+						}
+					} else {
+						book = "";
+					}
+					const verseMd =
+						//this is a comment to block the lint from moving the link to the next line
+						//TODO:Change link to be dynamic
+						`>[!bible] Verse of the Day - [ ${verseData.index}](${verseData.link}) ${book}
+						>${verseData.verse}</br> - ${verseData.index}\n`;
+					// Insert the verse at the current cursor position
+					editor.replaceSelection(verseMd);
+					console.log(verseData.verse);
+					console.log(verseData.index);
+				} else {
+					console.error("Failed to fetch verse data");
+				}
+
+				// editor.replaceRange(verse, editor.getCursor());
+			},
+		});
+		console.log("loading plugin");
+		this.registerEvent(
+			this.app.vault.on("create", () => {
+				console.log("a new file has entered the arena");
+			})
+		);
+	}
+
+	async onunload() {
+		console.log("unloading plugin");
+	}
+
+	async getVerseOfTheDay(
+		userLanguage: string
+	): Promise<{ verse: string; index: string; link: string } | null> {
+		// Construct the URL using string interpolation
+		const url = `https://www.bible.com/${userLanguage}/verse-of-the-day`;
+
+		return new Promise((resolve, reject) => {
+			https
+				.get(url, (response: IncomingMessage) => {
+					let data = "";
+
+					response.on("data", (chunk: string) => {
+						data += chunk;
+					});
+
+					response.on("end", () => {
+						// Use the specified pattern to match the desired text
+						const pattern =
+							/<div class="mbs-3 border border-l-large border-black pli-1 plb-1 pis-2">(.*?)<\/div>/s;
+						const match: RegExpMatchArray | null =
+							data.match(pattern);
+
+						if (match) {
+							const verseContainer: string = match[1].trim();
+							//console.log(verseContainer);
+
+							// Extract the verse from the divContent using slice
+							const slicedVerse: string = verseContainer.slice(
+								verseContainer.indexOf(">") + 1,
+								verseContainer.indexOf("</a>")
+							);
+							const verseIndex: string = verseContainer.slice(
+								verseContainer.indexOf('25">') + 4,
+								verseContainer.indexOf("</p")
+							);
+							const verseLinkContainer = verseContainer.slice(
+								verseContainer.indexOf("</a>") + 1,
+								verseContainer.indexOf("<p")
+							);
+							const verseLink: string =
+								"https://www.bible.com" +
+								verseLinkContainer.slice(
+									verseLinkContainer.indexOf("href=") + 6,
+									verseLinkContainer.indexOf('">')
+								);
+							resolve({
+								verse: slicedVerse,
+								index: verseIndex,
+								link: verseLink,
+							});
+						} else {
+							console.error("No match found for pattern.");
+							resolve(null);
+						}
+					});
+				})
+				.on("error", (error: any) => {
+					console.error("Error:", error);
+					reject(error);
+				});
+		});
+	}
+}
---
+++ settings.ts
@@ -0,0 +1,119 @@
+import verseOfTheDayPlugin from "main";
+import { App, PluginSettingTab, Setting } from "obsidian";
+
+export class VerseSettingsTab extends PluginSettingTab {
+	plugin: verseOfTheDayPlugin;
+	containerEl: HTMLElement; // Add containerEl property
+	constructor(app: App, plugin: verseOfTheDayPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display() {
+		const { containerEl } = this;
+		containerEl.empty(); // Clear the container if it's not empty
+		containerEl.createEl("h1", { text: "Verse of the Day Settings" });
+
+		new Setting(containerEl)
+			.setName("Language")
+			.setDesc("Language of the verse")
+			.addDropdown((dropdown: any) => {
+				dropdown.addOption("en", "English");
+				dropdown.addOption("af", "Afrikaans");
+				dropdown.addOption("am", "Amharic");
+				dropdown.addOption("ar", "Arabic");
+				dropdown.addOption("as", "Assamese");
+				dropdown.addOption("be", "Belarusian");
+				dropdown.addOption("bg", "Bulgarian");
+				dropdown.addOption("bn", "Bengali");
+				dropdown.addOption("ca", "Catalan");
+				dropdown.addOption("ckb", "Central Kurdish");
+				dropdown.addOption("cs", "Czech");
+				dropdown.addOption("cy", "Welsh");
+				dropdown.addOption("da", "Danish");
+				dropdown.addOption("de", "German");
+				dropdown.addOption("el", "Greek");
+				dropdown.addOption("en-GB", "English (United Kingdom)");
+				dropdown.addOption("eo", "Esperanto");
+				dropdown.addOption("es", "Spanish");
+				dropdown.addOption("es-ES", "Spanish (Spain)");
+				dropdown.addOption("et", "Estonian");
+				dropdown.addOption("fa", "Persian");
+				dropdown.addOption("fi", "Finnish");
+				dropdown.addOption("fr", "French");
+				dropdown.addOption("gu", "Gujarati");
+				dropdown.addOption("he", "Hebrew");
+				dropdown.addOption("hi", "Hindi");
+				dropdown.addOption("hr", "Croatian");
+				dropdown.addOption("ht", "Haitian Creole");
+				dropdown.addOption("hu", "Hungarian");
+				dropdown.addOption("hy", "Armenian");
+				dropdown.addOption("id", "Indonesian");
+				dropdown.addOption("ig", "Igbo");
+				dropdown.addOption("is", "Icelandic");
+				dropdown.addOption("it", "Italian");
+				dropdown.addOption("ja", "Japanese");
+				dropdown.addOption("ka", "Georgian");
+				dropdown.addOption("km", "Khmer");
+				dropdown.addOption("kn", "Kannada");
+				dropdown.addOption("ko", "Korean");
+				dropdown.addOption("lt", "Lithuanian");
+				dropdown.addOption("lv", "Latvian");
+				dropdown.addOption("mk", "Macedonian");
+				dropdown.addOption("ml", "Malayalam");
+				dropdown.addOption("mn", "Mongolian");
+				dropdown.addOption("mr", "Marathi");
+				dropdown.addOption("ms", "Malay");
+				dropdown.addOption("my", "Burmese");
+				dropdown.addOption("my-MM", "Burmese (Myanmar)");
+				dropdown.addOption("ne", "Nepali");
+				dropdown.addOption("nl", "Dutch");
+				dropdown.addOption("no", "Norwegian");
+				dropdown.addOption("pa", "Punjabi");
+				dropdown.addOption("pl", "Polish");
+				dropdown.addOption("pt", "Portuguese");
+				dropdown.addOption("pt-PT", "Portuguese (Portugal)");
+				dropdown.addOption("ro", "Romanian");
+				dropdown.addOption("ru", "Russian");
+				dropdown.addOption("si", "Sinhala");
+				dropdown.addOption("sk", "Slovak");
+				dropdown.addOption("sl", "Slovenian");
+				dropdown.addOption("sn", "Shona");
+				dropdown.addOption("sq", "Albanian");
+				dropdown.addOption("sr", "Serbian");
+				dropdown.addOption("sv", "Swedish");
+				dropdown.addOption("sw", "Swahili");
+				dropdown.addOption("ta", "Tamil");
+				dropdown.addOption("te", "Telugu");
+				dropdown.addOption("tg", "Tajik");
+				dropdown.addOption("th", "Thai");
+				dropdown.addOption("tl", "Tagalog");
+				dropdown.addOption("tr", "Turkish");
+				dropdown.addOption("uk", "Ukrainian");
+				dropdown.addOption("ur", "Urdu");
+				dropdown.addOption("uz", "Uzbek");
+				dropdown.addOption("vi", "Vietnamese");
+				dropdown.addOption("yo", "Yoruba");
+				dropdown.addOption("zu", "Zulu");
+				dropdown
+					.setValue(this.plugin.settings.language)
+					.onChange((value: any) => {
+						this.plugin.settings.language = value;
+						this.plugin.saveSettings();
+					});
+			});
+		new Setting(containerEl)
+			.setName("Outgoing Link")
+			.setDesc(
+				"Adds a outgoing link to the header that leads to another file inside your vault, for example [[John]]"
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.outgoingLink)
+					.onChange((value) => {
+						this.plugin.settings.outgoingLink = value;
+						this.plugin.saveSettings();
+					})
+			);
+	}
+}
